07afd91192f717286cbeb93536e51b350739f545
Chiara Boschiero <boschierochiara@gmail.com>
2020-02-11 12:21:44 +0100

    database setup

--- test/dao/AmministratoreDAOImplTest.java
+++ test/dao/AmministratoreDAOImplTest.java
@@ -21,41 +21,37 @@ class AmministratoreDAOImplTest {
 			Utente u = new Utente("mario92", "password", "Mario", "Rossi", new java.util.Date(),
 					"mario.rossi@gmail.com", "338 1234567", true);
 
-//                 dao.insert(u);
-//			
-//			// u dovrebbe essere nel db!
-//			
-             Utente result = dao.select("mario92");
-//			
-//			assertEquals(u.getIdUtente(), result.getIdUtente());
-//			assertEquals(u.getPassword(), result.getPassword());
-//			assertEquals(u.getNome(), result.getNome());
-//			assertEquals(u.getCognome(), result.getCognome());
-//			assertEquals(u.getDataNascita(), result.getDataNascita());
-//			assertEquals(u.getEmail(), result.getEmail());
-//			assertEquals(u.getTelefono(), result.getTelefono());
-//			
-//			u.setCognome("Bianchi");
-//			dao.update(u);
-//			
-//			result = dao.select("mario92");
-//			assertEquals(u.getCognome(), result.getCognome());
-			
-			
-//		
-//			
-//			dao.delete("mario92");
-//			
+			dao.insert(u);
+
+			// u dovrebbe essere nel db!
+
+			Utente result = dao.select("mario92");
+
+			assertEquals(u.getIdUtente(), result.getIdUtente());
+			assertEquals(u.getPassword(), result.getPassword());
+			assertEquals(u.getNome(), result.getNome());
+			assertEquals(u.getCognome(), result.getCognome());
+			// assertEquals(u.getDataNascita(), result.getDataNascita());
+			assertEquals(u.getEmail(), result.getEmail());
+			assertEquals(u.getTelefono(), result.getTelefono());
+
+			u.setCognome("Bianchi");
+			dao.update(u);
+
+			result = dao.select("mario92");
+			assertEquals(u.getCognome(), result.getCognome());
+
+			dao.delete("mario92");
 		} catch (Exception ex) {
-			fail("UEx" + ex.getMessage());
+			fail("Unexpected Exception: " + ex.getMessage());
 		}
-		
-//	try {
-//		dao.select("mario92");
-//			fail("Select should have failed here!");
-//		} catch (SQLException e) {
-//			//as expected
-//		}		
-		
+
+		try {
+			dao.select("mario92");
+			fail("Select should have failed here!");
+		} catch (SQLException e) {
+			// as expected
+		}
+
 	}
 }
